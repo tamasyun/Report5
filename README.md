6a14a159fb3261c2d4b23fe972044375958b0caa
Shunsuke TAMASHIRO <e245732@ie.u-ryukyu.ac.jp>
2024-12-11 17:43:41 +0900

    step2

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # 課題レポート5
 ## ステップ1: コードの準備。
 - String型の変数strにnullを代入する。
-- str.length()を表示させようとして例外が発生する
+- str.length()を表示させようとして例外が発生する。
+## ステップ2: 例外処理を記述せよ。
+- 「NullPointerException」を catchする。
+- NullPointerExceptionが発生したことを標準出力する。
+- 例外インスタンスを用いて、getMessage()を出力する。
--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
